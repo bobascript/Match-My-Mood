f0b04016a5261409ac8d294f3c286e8a3a755ed7
Lisette33 <113862182+Lisette33@users.noreply.github.com>
2023-03-13 21:11:58 -0400

    Added and updated login for js and css

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './logo.svg';
 import Homepage from './components/Homepage.js';
+import Header from './components/Header';
+import Login from './components/Login';
+
+
 
 function App() {
   return (
