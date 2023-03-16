195159be4088efeaab091f6a8297ca2d1a5f7ad3
mintghostii <alvarezjavi960@gmail.com>
2023-03-16 19:47:54 -0400

    updated player to have our text area and increased mobile compatibility

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../styles/Homepage.css';
-import Header from '../components/Header.js';
 // import Happy from '../assets/happypic.png';
 // import Calm from '../assets/calmpic.png';
 // import Sad from '../assets/sadpic.png';
@@ -11,7 +10,6 @@ function Homepage(){
     return (
           <div>
     <div class="d-flex flex-column justify-content-center w-100 h-100">
-        <Header/>
       <div style={{display:"flex",flex:"column",justifyContent:"center",width:"100%",height:"100%"}}>
         <button className="loginBtn" >Log in /<br/>Sign up!</button>
       </div>
--- client/src/components/Moods.js
+++ client/src/components/Moods.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../styles/Moods.css';
-import Header from '../components/Header.js';
 import Happy from '../assets/happypic.png';
 import Calm from '../assets/calmpic.png';
 import Sad from '../assets/sadpic.png';
@@ -10,7 +9,6 @@ function Moods() {
     return (
         <div>
         <div class="d-flex flex-column justify-content-center w-100 h-100">
-            <Header/>
           <div style={{display:"flex",flex:"column",justifyContent:"center",width:"100%",height:"100%"}}>
             <button className="loginBtn" >Log in /<br/>Sign up!</button>
           </div>
