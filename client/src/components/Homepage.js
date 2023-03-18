765466ae7acbc52fe195c2605808eeaaa92cc14c
areedy <a.reedy@gmail.com>
2023-03-18 11:59:24 -0400

    added apollo wrapper to app.js, started functionality for player and homepage

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/Homepage.css';
 import Header from '../components/Header.js';
+import Player from './Player';
 // import Happy from '../assets/happypic.png';
 // import Calm from '../assets/calmpic.png';
 // import Sad from '../assets/sadpic.png';
@@ -12,7 +13,7 @@ function Homepage(){
       <div>
 
       <div class="d-flex flex-column justify-content-center w-100 h-100">
-          <Header/>
+          <Player/>
         <div style={{display:"flex",flex:"column",justifyContent:"center",width:"100%",height:"100%"}}>
         </div>
       </div>
