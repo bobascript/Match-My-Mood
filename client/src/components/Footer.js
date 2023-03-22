39e29a11c7b88eae3b8ce80f6760c934425a9a27
Andrew <andrewg87@gmail.com>
2023-03-21 22:03:54 -0400

    found more instances of class, changed them to className

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,10 +6,10 @@ function Footer(){
         <footer className="footer">
             <div style={{justifySelf:"center",position:"absolute",fontSize:"25px"}}>Find us here!</div>
             <ul>
-                <li><button><a target={{}} href="https://www.github.com/bobascript"><i class="fa-brands fa-github fa-3x"/></a>Javier</button></li>
-                <li><button><a target={{}} href="https://www.github.com/graciaan"><i class="fa-brands fa-github fa-3x"/></a>Andrew</button></li>
-                <li><button><a target={{}} href="https://www.github.com/Lisette33"><i class="fa-brands fa-github fa-3x"/></a>Lisette</button></li>
-                <li><button><a target={{}} href="https://www.github.com/asreedy82"><i class="fa-brands fa-github fa-3x"/></a>Anthony</button></li>
+                <li><button><a target={{}} href="https://www.github.com/bobascript"><i className="fa-brands fa-github fa-3x"/></a>Javier</button></li>
+                <li><button><a target={{}} href="https://www.github.com/graciaan"><i className="fa-brands fa-github fa-3x"/></a>Andrew</button></li>
+                <li><button><a target={{}} href="https://www.github.com/Lisette33"><i className="fa-brands fa-github fa-3x"/></a>Lisette</button></li>
+                <li><button><a target={{}} href="https://www.github.com/asreedy82"><i className="fa-brands fa-github fa-3x"/></a>Anthony</button></li>
             </ul>
         </footer>
     )
